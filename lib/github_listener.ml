e949bea88b3af1ac9c2916e6f334b73535846d9c
David Sheets <sheets@alum.mit.edu>
2013-04-24 12:12:41 +0100

    remove exn handler; improve error messages; add work queue argument

--- lib/github_listener.ml
+++ lib/github_listener.ml
@@ -20,7 +20,10 @@ let notify_uriref (user, repo) = sprintf "%s/%s?notify" user repo
 let watch_list = ["ocamlot","opam-repository"]
 let registry = Hashtbl.create 10
 
-let make_listener service_fn ~root ~host ~port =
+let github_error_str (user, repo) =
+  sprintf "GitHub connection for %s/%s failed:" user repo
+
+let make_listener service_fn ~root ~host ~port ~queue =
   let base = Uri.make ~scheme:"http" ~host ~port ~path:("/"^root^"/") () in
   let routes = Re.(seq [str root; char '/'; notify_re]) in
   let path_match = Re.compile routes in
@@ -51,32 +54,41 @@ let make_listener service_fn ~root ~host ~port =
     ~routes
     ~handler
     ~startup:(List.map (fun watch ->
-      catch begin
-        fun () -> Persist.get ~access:`Private token_kp
-        >>= begin function
-          | Some (_, _, token) -> return token
-          | None -> (Github.(Monad.(run (
-            github
-            >> Token.create
-              ~note:github_ua ~note_url:(Uri.to_string base)
-              ~client_id:Config.github_app_id
-              ~client_secret:Config.github_app_secret
-              ~user:Config.github_user
-              ~pass:Config.github_pass ()
-            >>= fun auth -> return (Token.of_auth auth))))
-          ) >>= fun token ->
-              Persist.put token_kp token
-            >>= fun _ -> return token
-        end
-        >>= fun token ->
-          let github = Github.(Monad.(github >> API.set_token token)) in
-          let url = Uri.resolve "http" base
-            (Uri.of_string (notify_uriref watch)) in
-          Github_hook.connect github registry url (watch,notification_handler)
-        >>= fun endpoint -> return () (* TODO: SYNC *)
-        end
-        begin fun exn ->
-          eprintf "GitHub connection failed: %s\n%!" (Printexc.to_string exn);
-          return ()
-        end
+      Persist.get ~access:`Private token_kp
+      >>= begin function
+        | Some (_, _, token) -> return token
+        | None -> (Github.(Monad.(run (
+          github
+          >> Token.create
+            ~note:github_ua ~note_url:(Uri.to_string base)
+            ~client_id:Config.github_app_id
+            ~client_secret:Config.github_app_secret
+            ~user:Config.github_user
+            ~pass:Config.github_pass ()
+          >>= fun auth -> return (Token.of_auth auth))))
+        ) >>= fun token -> (
+          Persist.put token_kp token
+          >>= fun _ -> return token
+        ) end
+      >>= fun token ->
+      let github = Github.(Monad.(github >> API.set_token token)) in
+      let url = Uri.resolve "http" base (Uri.of_string (notify_uriref watch)) in
+      Github_hook.(
+        connect github registry url (watch,notification_handler)
+        >>= fun endpoint -> match endpoint with
+          | {status=Indicated} ->
+              eprintf "%s wedged prerequest\n%!" (github_error_str watch);
+              return ()
+          | {status=Pending} ->
+              eprintf "%s wedged pending\n%!" (github_error_str watch);
+              return ()
+          | {status=Timeout} ->
+              eprintf "%s handshake timeout\n%!" (github_error_str watch);
+              return ()
+          | {status=Unauthorized} ->
+              eprintf "%s authorization failed\n%!" (github_error_str watch);
+              return ()
+          | {status=Connected} ->
+              return ()
+      )
     ) watch_list)
