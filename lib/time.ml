dbc086ddc4cda468355f1cf088fa3a4241c6c190
David Sheets <sheets@alum.mit.edu>
2013-05-02 22:47:58 +0100

    some time functions

--- lib/time.ml
+++ lib/time.ml
@@ -3,10 +3,15 @@ type duration = float
 
 let min = 0.
 let now () = Unix.gettimeofday ()
-let to_string t = Unix.(
-  let tm = gmtime t in
-  Printf.sprintf "%d-%02d-%02dT%02d:%02d:%02dZ"
-    (tm.tm_year + 1900) (tm.tm_mon + 1) tm.tm_mday
+let date_to_string_ tm = Unix.(
+  Printf.sprintf "%d-%02d-%02d" (tm.tm_year + 1900) (tm.tm_mon + 1) tm.tm_mday
+)
+let to_string_ tm = Unix.(
+  Printf.sprintf "%sT%02d:%02d:%02dZ"
+    (date_to_string_ tm)
     tm.tm_hour tm.tm_min tm.tm_sec
 )
+let date_to_string t = date_to_string_ (Unix.gmtime t)
+let duration_to_string t = Printf.sprintf "%.2fs" t
+let to_string t = to_string_ (Unix.gmtime t)
 let elapsed t_0 t_1 = abs_float (t_0 -. t_1)
--- lib/time.mli
+++ lib/time.mli
@@ -4,4 +4,6 @@ type duration
 val min : t
 val now : unit -> t
 val to_string : t -> string
+val date_to_string : t -> string
+val duration_to_string : duration -> string
 val elapsed : t -> t -> duration
