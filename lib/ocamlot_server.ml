6e0bd6befb2b095ce3ed9e53ea0069c47feec668
David Sheets <sheets@alum.mit.edu>
2013-05-30 11:27:30 +0100

    daemonize and functionalize server

--- lib/ocamlot_server.ml
+++ lib/ocamlot_server.ml
@@ -1,38 +1,39 @@
-open Config
-
 let watch_list = [
   "ocamlot", "opam-repository";
   "ocamlot-dev", "opam-repository";
 ]
 
-let base = Uri.make ~scheme:"https" ~host ~port:outside_port ~path:"/" ()
+let daemon base port =
+  let ocamlot = Ocamlot.make ~base in
 
-let ocamlot = Ocamlot.make ~base
+  let browser_listener = Ocamlot.browser_listener
+    (Http_server.service "Browser Request Listener")
+    ~base ocamlot in
 
-let browser_listener = Ocamlot.browser_listener
-  (Http_server.service "Browser Request Listener")
-  ~base ocamlot
+  let worker_listener = Ocamlot.worker_listener
+    (Http_server.service "Worker Task Queue Listener")
+    ~base ocamlot in
 
-let worker_listener = Ocamlot.worker_listener
-  (Http_server.service "Worker Task Queue Listener")
-  ~base ocamlot
+  let gh_listener = Github_listener.make_listener ocamlot in
+  let gh_event_service = Github_listener.service gh_listener
+    (Http_server.service "GitHub Listener") in
 
-let gh_listener = Github_listener.make_listener ocamlot
-let gh_event_service = Github_listener.service gh_listener
-  (Http_server.service "GitHub Listener")
+  let http_server = Http_server.make_server host port in
+  let gh_http_server = Http_server.register_service http_server
+    Github_listener.(gh_event_service
+                       ~startup:(List.map
+                                   (fun (user, repo) ->
+                                     attach gh_listener ~user ~repo)
+                                   watch_list)) in
+  let gh_event_server = Http_server.register_service
+    gh_http_server browser_listener in
+  let ocamlot_server = Http_server.register_service
+    gh_event_server worker_listener in
 
-let http_server = Http_server.make_server host port
-let gh_http_server = Http_server.register_service http_server
-  Github_listener.(gh_event_service
-                     ~startup:(List.map
-                                 (fun (user, repo) ->
-                                   attach gh_listener ~user ~repo)
-                                 watch_list))
-let gh_event_server = Http_server.register_service
-  gh_http_server browser_listener
-let ocamlot_server = Http_server.register_service
-  gh_event_server worker_listener
-;;
-Lwt_unix.run (Lwt_list.iter_p (fun x -> x) [
-  Http_server.(run ocamlot_server);
-])
+  Lwt_daemon.daemonize
+    ~stdout:`Keep ~stderr:`Keep
+    ~directory:(Unix.getcwd ())
+    ();
+  Lwt_unix.run (Lwt_list.iter_p (fun x -> x) [
+    Http_server.(run ocamlot_server);
+  ])
