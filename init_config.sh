c159ba4ac5e1a41b01eef4b3c317c686c7883644
David Sheets <sheets@alum.mit.edu>
2013-06-10 11:22:52 +0100

    initialize state from github

--- init_config.sh
+++ init_config.sh
@@ -2,5 +2,9 @@
 if [ ! -e ./lib/config.ml ]; then
   cp ./lib/config.ml.in ./lib/config.ml
 fi
-mkdir -p store/public
-mkdir -p store/private
+if [ ! -e ./state ]; then
+  git clone git@github.com:ocamlot/ocamlot-state.git state
+  cd state
+  git config --local --add user.name ocamlot
+  git config --local --add user.email infrastructure@lists.ocaml.org
+fi
