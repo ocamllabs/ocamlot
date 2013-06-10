ec9984ab073cfb02319ff650c8291d0059170e00
David Sheets <sheets@alum.mit.edu>
2013-06-10 18:07:03 +0100

    clean before configure

--- install_ocaml.sh
+++ install_ocaml.sh
@@ -7,7 +7,7 @@ OCAML_SRC_DIR=$2
 mkdir -p $OCAML_PREFIX_DIR
 
 cd $OCAML_SRC_DIR
-./configure -prefix $OCAML_PREFIX_DIR/$OCAML_SLUG
 make distclean
+./configure -prefix $OCAML_PREFIX_DIR/$OCAML_SLUG
 make world.opt
 make install
