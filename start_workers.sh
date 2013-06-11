7a00f8688e68cb56544117e9b40cd4f17e05f107
David Sheets <sheets@alum.mit.edu>
2013-06-11 15:21:50 +0100

    start_worker worker names and logging

--- start_workers.sh
+++ start_workers.sh
@@ -3,6 +3,8 @@
 DIR=`dirname $0`
 WORKERS=$1
 URL=$2
+mkdir -p log
 for i in $(seq 1 $WORKERS); do
-  tmux new -d "$DIR/ocamlot_cmd.native work $URL"
+  tmux new -d -s worker$1 \
+      "$DIR/ocamlot_cmd.native work $URL 2> log/worker$i.err > log/worker$i.out"
 done
