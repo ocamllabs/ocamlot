e55fd57cae089a489471343b6ebe7f96b8b6007c
David Sheets <sheets@alum.mit.edu>
2013-06-20 15:58:11 +0200

    Update setup_deps.sh
    After merges

--- setup_deps.sh
+++ setup_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 opam pin github git://github.com/avsm/ocaml-github
-opam pin cohttp git://github.com/dsheets/ocaml-cohttp#magic-biscuit
+opam pin cohttp git://github.com/avsm/ocaml-cohttp
 opam pin cow git://github.com/mirage/ocaml-cow
 
