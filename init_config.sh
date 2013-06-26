51dca74d61f258b10ffcf2c357bfcfe39e935386
David Sheets <sheets@alum.mit.edu>
2013-06-26 17:46:00 +0100

    Checkout the branch we care about

--- init_config.sh
+++ init_config.sh
@@ -9,11 +9,8 @@ fi
 if [ ! -e $STATE ]; then
   mkdir $STATE
   cd $STATE
-  git clone git://github.com/ocamlot/ocamlot-state.git .
+  git clone -b $BRANCH git@github.com:ocamlot/ocamlot-state.git .
   git config --local --add user.name ocamlot
   git config --local --add user.email dwws2@cam.ac.uk
   cd $PWD
 fi
-cd $STATE
-git pull origin $BRANCH
-git checkout -b $BRANCH
